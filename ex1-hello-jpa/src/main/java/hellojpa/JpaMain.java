9b5387db46d932a766e23b1f0e7ba95b53523d28
kangsw9395 <kangsw9395@gmail.com>
2020-07-16 23:04:28 +0900

    CRUD에 대한 간단한 내용 작성

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,12 +19,16 @@ public class JpaMain {
 
         tx.begin();
         try{
-            Member member = new Member();
-//        member.setId(1L);
-//        member.setName("HelloA");
-            member.setId(2L);
-            member.setName("HelloB");
-            em.persist(member);
+            //entityManager를 자바 컬렉션처럼 이해하면된다. -> 내 객체를 대신 저장해주는 애
+            Member findMember = em.find(Member.class,1L); //pk가 1인 애를 조회
+            //em.remove(findMember); //삭제
+
+            //수정
+            findMember.setName("HelloJPA");
+            //자바 객체에서 값만 수정했는데도, Update 쿼리가 나간다.
+            // JPA를 통해서 entity를 가져오면 JPA가 관리한다. 트랜잭션 commit 시점에 확인하고, 바뀌어있으면 Update 쿼리가 나간다.
+
+
             tx.commit();
         }catch(Exception e){
             tx.rollback();
