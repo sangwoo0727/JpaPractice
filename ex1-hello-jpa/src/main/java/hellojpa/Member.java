d181436c62ce3bb7125c294c69f1f57543c7b572
kangsw9395 <kangsw9395@gmail.com>
2020-07-21 14:49:00 +0900

    테이블 전략- 키 생성 전용 테이블을 하나 만들어서 데이터베이스 시퀀스를 흉내내는 전략

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,26 +6,31 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-//@Table(name = "Member") //이런 경우는 생략 가능
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES", //테이블 이름
+        pkColumnName = "MEMBER_SEQ", allocationSize = 1) //PK 컬럼명
 public class Member {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.TABLE,
+                    generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
+
     //기본키를 직접 할당할 때는 @Id만 사용
     // @GeneratedValue는 autoIncrement처럼 데이터베이스가 기본키를 생성하도록 위임하는 것.
 
     @Column(name = "name", nullable = false)
     private String username;
 
-    public Member() {
-    }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(String id) {
+
+    public void setId(Long id) {
         this.id = id;
+    }
+    public Member() {
     }
 
     public String getUsername() {
